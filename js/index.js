ff29c6f32d632f0ac1cf9abf39be7031f3b76923
dewarian <nathanbommezijn@gmail.com>
2021-11-09 15:24:29 +0100

    :test_tube: Add test of DOM manipulation

--- js/index.js
+++ js/index.js
@@ -8,4 +8,30 @@ function getData() {
     .then(data => console.table(data))
 }
 
-getData()
+
+/* Select the element div and set the text or html */
+// d3.select('text').text('Hello') //Doesn't do anything?
+d3.select('div').text('Hello')
+
+const svg = d3.select('svg')
+svg.style("border", "1px solid black")
+
+/* const circle = d3.select('circle')
+circle
+  .attr("r", 10)
+  .attr("cx", 10)
+  .attr("cy", 10)
+  .style("stroke-width", 2)
+  .style("fill", '#ffcc00') */
+
+for (let index = 0; index < 3; index++) {
+  let coords = Math.round(Math.random()*100)
+  svg.append('circle')
+    .attr("r", 10)
+    .attr("cx", coords)
+    .attr("cy", coords*2)
+    .style("stroke-width", 2)
+    .style("stroke", "black")
+    .style("fill", '#ffcc00')
+  
+}
