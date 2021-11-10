cc7a19cc51e90a55883271330ab48755eeba10e4
dewarian <nathanbommezijn@gmail.com>
2021-11-10 17:02:29 +0100

    :fire: Got some bar chart with my own data

--- js/index.js
+++ js/index.js
@@ -7,30 +7,45 @@ const dimensions = {
 }
 
 const FAKEDATA = [33, 88, 54, 102, 55];
+async function main() {
+  const data = d3.json('https://api.themoviedb.org/3/person/popular?api_key=63b8e2e812b6172f220bb5bb9aab2dea')
+    .then(
+      data => {
+        return data.results.map(x => {
+        const actor = {
+          name: x.name,
+          mugshot: x.profile_path,
+          rating: x.popularity
+        }
+        return actor
+      })
+    })
 
-function getData() {
-  fetch(`${URL}${endpoint}`)
-    .then(response => response.json())
-    .then(data => console.table(data))
-}
+  const SVG = d3.select('body').append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+    .attr('border', '1px solid black')
 
-const SVG = d3.select('body').append('svg')
-  .attr('width', dimensions.width)
-  .attr('height', dimensions.height)
-  .attr('border', '1px solid black')
+  const GROUP = SVG.append('g')
+    .attr('transform', `translate(${dimensions.margin.l}, ${dimensions.margin.t})`)
 
-const GROUP = SVG.append('g')
-  .attr('transform', `translate(${dimensions.margin.l}, ${dimensions.margin.t})`)
 
-const RECTANGLE =
-  GROUP.selectAll('rect').data(FAKEDATA).join(
-    (enter) => enter.append('rect').attr('x',0),
-    (update) => update,
-    (exit) => exit.remove()
-  );
+    const RECTANGLE =
+    GROUP.selectAll('rect')
+      .data(await data)
+      .join(
+        (enter) => enter.append('rect').attr('x',0),
+        (update) => update,
+        (exit) => exit.remove()
+    );
 
-  RECTANGLE
+    RECTANGLE
     .attr('height', 50)
-    .attr('width', (d) => d * 7)
+    .attr('width', (d) => d.rating * 7)
     .attr('y', (d,i) => i*(50+5))
     .attr('class', 'bars')
+}
+
+main()
+
+
