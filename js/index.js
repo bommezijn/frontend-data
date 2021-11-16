96a998868902c16c02421e03bacf41b8eca98885
dewarian <nathanbommezijn@gmail.com>
2021-11-17 00:10:49 +0100

    :poop: Half a rewrite
    Current issues are as following
    * Bars take whole svg
      * Bars look like taking the horizontal axis instead of vertical.
      * It is ugly

--- js/index.js
+++ js/index.js
@@ -1,108 +1,97 @@
-console.log(`%cHello World`, `color: #ffcc00; font-weight: bold;`)
-import getData from './modules/data.js';
-import {dim, createSVG} from './modules/svg.js'
-
-// const WIDTH = 600, HEIGHT = 800;
-const SVG = createSVG()
-
+console.log(`%cHello JS`, `color: #ffcc00; font-weight: bold;`)
 const API = 'https://api.themoviedb.org/3/person/popular?api_key=63b8e2e812b6172f220bb5bb9aab2dea'
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
 
-const dataset = getData(API).then(whole =>
-  whole.map(specified => {
-    const CELEB = {
-      name: specified.name,
-      rating: Math.round(specified.popularity),
-      mug: `https://image.tmdb.org/t/p/w500/${specified.profile_path}`
-    }
-    return CELEB
-  })
-).catch(e => {
-  console.error(e);
-  d3.select('body').append('p').text('error in data fetching')
-})
-
-// const createGroup = SVG.append('g')
-const createGroup = (parent) => {
-  return parent.append('g')
-    // .attr('transform', `translate(${dim.margin.l} ${dim.margin.t})`)
+const dimensions = {
+  margin: {t:20, r: 20, b: 30, l: 40},
+  get width() {return 960 - this.margin.l - this.margin.r},
+  get height() {return 500 - this.margin.t - this.margin.b}
 }
+
+const SVG = d3.select('body').append('div').attr('id', 'graph').append('svg')
+  .attr('width', dimensions.width + dimensions.margin.l + dimensions.margin.r)
+  .attr('height', dimensions.height + dimensions.margin.t + dimensions.margin.b)
+  .append('g')
+    .attr('transform', `translate(${dimensions.margin.l}, ${dimensions.margin.t})`)
+
 
 const createAxis = (data) => {
   const X = d3.scaleBand()
-    .range([0, dim.width])
-    .domain(data.map(d => d.name))
-    .padding(0.2)
-
-  SVG.append('g')
-    .attr('transform', `translate(0, ${dim.height})`)
-    .call(d3.axisBottom(X))
-    .selectAll('text')
-      .attr('transform', `translate(-10,0)rotate(-45)`)
-      .style('text-anchor', 'end')
+    .domain(data.map(d => d.family))
+    .range([0, dimensions.width])
 
   const Y = d3.scaleLinear()
-    .range([dim.height, 0])
-    .domain([0, d3.max(data, d => {return d.rating})])
+    // .domain([0, d3.max(data, d=> {return d.rating})]) 
+    .domain(d3.extent(data, d => d.rating))
+    .range([dimensions.height, 0]);
 
   SVG.append('g')
-    .call(d3.axisLeft(Y))
-  /* Create X axis based on the value from rating */
-  const X_AXIS = d3.scaleLinear()
-    .domain(d3.extent(data, d => d.rating))
-    .range([0, dim.width]);
-
-  // // Add X axis to the bottom axis
-  // SVG.append('g')
-  // .attr('transform', `translate(0,  ${dim.height - dim.margin.t})`) //push scale down
-  // .call(d3.axisBottom(X_AXIS))
+    .attr('transform', `translate(0, ${dimensions.height})`)
+    .call(d3.axisBottom(X))
+    .selectAll('text')
+    .attr('transform', `translate(-10,0) rotate(-45)`)
+    .style('text-anchor', 'end')
   
-  // /* Create Y axis based on the value from name */
-  // const Y_AXIS = d3.scaleBand() //shouldn't be ordinal but scaleband, not sure why but it works.
-  //   .domain(data.map(d => {return d.name}))
-  //   .range([0, dim.height]) //Depending on the starting value the data gets sorted U -> D or D-> U && 50 to center the label with bar
+  SVG.append('g')
+    .call(d3.axisLeft(Y))
 
-  // // Add Y axis to the right
-  // SVG.append('g')
-  // .attr('transform', `translate(${dim.width - dim.margin.r})`)
-  //   .call(d3.axisLeft(Y_AXIS))
-
-  // return {X_AXIS, Y_AXIS};
-  return {X, Y};
+  return { X, Y};
 }
 
-const render = async (dataset) => {
-  const {X, Y} = createAxis(await dataset) 
-  // SVG.attr('transform', `translate(20, 20)`) //Padding to remove the SVG from the corner of the screen
-  // console.log(await dataset)
-  const rectangles =  createGroup(SVG)
-    .selectAll('rect')
-    .data(await dataset)
-    .join(
-      (enter) => {
-        const r = enter.append('rect')
-        r.append('title')
-        return r
-      },
-      (update) => update,
-      (exit) => exit.remove()
-    )
-  
-  rectangles
-    // .attr('height', 20) //a predefined value for the height of a single data bar
-    // .attr('height', (dim.height / Y.bandwidth())) //a predefined value for the height of a single data bar
-    .attr('height', d => dim.height - Y(d.rating)) //a predefined value for the height of a single data bar
-    // .attr('width', (d) => {return dim.width * (`0.${d.rating}`)})
-    .attr('width', X.bandwidth())
-    .attr('x', d => X(d.name))
-    .attr('y', d => Y(d.rating)) //The 5 is the padding between. Should be done differently
-    .attr('fill', '#B61544')
-    .select('title').text(d => {return d.rating})
+const dataset = async (url) => {
+  try {
+    const data = d3.json(url)
+    return await data
+  } catch (error) {
+    console.error(error)
   }
+}
 
-render(await dataset)
+const roundNumber = (Number) => {
+  return typeof(Number) === 'number' ? Math.round(Number) : console.error('cannot convert non numerical value')
+}
 
-// note to future self, I added Y_AXIS.bandwith() to line 74 and 77. This made all the bars fill the height of the entire group and making it equal to the axisLeft.
-// Currently there is this issue with the width of the graph, it does not correlate to the bars or the axis. This has to be fixed.
-// Another thing that has to be fixed is the positioning of the graph itself and the axis 
-// Besides that it needs interactivity, thus I have to find something within the dataset to compare or filter to. an Idea would be to get the best movies or somethign
+const createPath = (IMAGE_URL, slug) => {
+  return `${IMAGE_URL}${slug}`
+}
 
+const numberToGender = (Number) => {
+  return Number === 1 ? 'Female' : 'Male'
+}
+
+const sanitizeData = async (data) => {
+  const dirtyData = await data.results;
+  try {
+    return dirtyData.map(
+      selection => {
+        const ACTOR = {
+          name: selection.name,
+          rating: roundNumber(selection.popularity),
+          gender: numberToGender(selection.gender),
+          photo: createPath(IMAGE_URL, selection.profile_path)
+        }
+        return ACTOR
+      }
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log(await sanitizeData(await dataset(API)))
+
+async function render(data) {
+  const {X, Y} = createAxis(data)
+
+  SVG.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d) => {return X(d.name)})
+    .attr('width', X.bandwidth())
+    .attr('y', (d) => {return Y(d.rating)})
+    .attr('height', (d) => {return dimensions.height - Y(d.rating)})  
+    .style('border', `1px solid black`)
+}
+
+render(await sanitizeData(await dataset(API)))
