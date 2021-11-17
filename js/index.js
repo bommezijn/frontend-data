1a07172bf706d7cefa9335cf0bfaeb24ec209264
dewarian <nathanbommezijn@gmail.com>
2021-11-17 23:01:02 +0100

    :bulb: Write comments for the creation of the SVG

--- js/index.js
+++ js/index.js
@@ -2,19 +2,25 @@ console.log(`%cHello JS`, `color: #ffcc00; font-weight: bold;`)
 import {sanitizeData, dataset} from './modules/data.js'
 const API = 'https://api.themoviedb.org/3/person/popular?api_key=63b8e2e812b6172f220bb5bb9aab2dea'
 
+//Define dimensions used for the SVG, width and height are a calculation, so for objects its a getter function
 const dimensions = {
   margin: {t:20, r: 20, b: 30, l: 40},
   get width() {return 960 - this.margin.l - this.margin.r},
   get height() {return 600 - this.margin.t - this.margin.b}
 }
 
+/* 
+  SVG variable is a d3.select chain which creates a div for the graph with an SVG in it and gives the following attributes
+  * Width, calculation of width + margin left + margin right
+  * Height, calculation of width + margin top + margin bottom + 50 for extra space
+  And creates a group within the svg (with a translate to simulate a margin and padding within the svg)
+*/
 const SVG = d3.select('body').append('div').attr('id', 'graph').append('svg')
   .attr('width', dimensions.width + dimensions.margin.l + dimensions.margin.r)
   .attr('height', dimensions.height + dimensions.margin.t + dimensions.margin.b + 50)
-  .style('border', '1px solid black')
+  // .style('border', '1px solid black')
   .append('g')
     .attr('transform', `translate(${dimensions.margin.l}, ${dimensions.margin.t})`)
-
 
 const createAxis = (data) => {
   const X = d3.scaleBand()
