951666e4217ca328dfe7e5a8fd765a0b52bf5df0
dewarian <nathanbommezijn@gmail.com>
2021-11-18 22:23:22 +0100

    :construction: Bars and scale update without re-rendering

--- js/index.js
+++ js/index.js
@@ -25,79 +25,104 @@ const SVG = d3.select('body').append('div').attr('id', 'graph').append('svg')
     .attr('transform', `translate(${dimensions.margin.l}, ${dimensions.margin.t})`)
 
 /**
+   * @title CURRENTLY DISABLED CUZ OF AN ISSUE
    * @description Creates Axis and scales for the graph. Specifically a scaleBand and scaleLinear
    * @see {@link https://github.com/suwigyarathore/chart-examples/blob/555e608e78bedeac0fe4dcc541819236c08cdc02/scatter-line/main.js#L30} for reference of the codestyle from suwigyarathore.
    * @param {JSON} data dataset that the graph uses to generate the scaleBand and scaleLinear
    * @returns Methods X and Y
  */
-const createAxis = (data) => {
-  const X = d3.scaleBand() //scale for ordinal or categorical dimensions.
-    .domain(data.map(d => d.name)) //Data it needs to map to the ranges
-    .range([0, dimensions.width]) //Plots domain elements to a 'physical' location on the graph
-    .paddingInner(0.2)
+// const createAxis = (data) => {
+//   const X = d3.scaleBand() //scale for ordinal or categorical dimensions.
+//     .domain(data.map(d => d.name)) //Data it needs to map to the ranges
+//     .range([0, dimensions.width]) //Plots domain elements to a 'physical' location on the graph
+//     .paddingInner(0.2)
 
-    SVG.append('g')
-    .attr('class', 'xAxis')
-    .attr('transform', `translate(0, ${dimensions.height})`)
-    .call(d3.axisBottom(X)) //Plot X axis (scaleBand) to the bottom
-    .attr('fill', '#ffffff20')
-    .selectAll('text')
-      .attr('transform', `translate(-10,0) rotate(-45)`) //Rotate text for easier reading.
-      .attr('fill', 'white')
-      .style('text-anchor', 'end')
+//     SVG.append('g')
+//     .attr('class', 'xAxis')
+//     .attr('transform', `translate(0, ${dimensions.height})`)
+//     .call(d3.axisBottom(X)) //Plot X axis (scaleBand) to the bottom
+//     // .attr('fill', '#ffffff20')
+//     .selectAll('text')
+//       .attr('transform', `translate(-10,0) rotate(-45)`) //Rotate text for easier reading.
+//       // .attr('fill', 'white')
+//       .style('text-anchor', 'end')
 
-  const Y = d3.scaleLinear()
-    .domain([0, d3.max(data, d=> {return d.rating})]) //Create a horizontal scale from 0 to the max value of the dataset. Use max instead of extent for the sake of 0 start value
-    .range([dimensions.height, 0]);
+//   const Y = d3.scaleLinear()
+//     .domain([0, d3.max(data, d=> {return d.rating})]) //Create a horizontal scale from 0 to the max value of the dataset. Use max instead of extent for the sake of 0 start value
+//     .range([dimensions.height, 0]);
 
     
-    SVG.append('g')
-    .attr('class', 'yAxis')
-    .call(d3.axisLeft(Y)) //Plot Y axis (scaleLinear) to the left
-    .attr('fill', '#ffffff20')
-      .selectAll('text')
-      .attr('fill', 'white')
+//     SVG.append('g')
+//     .attr('class', 'yAxis')
+//     .call(d3.axisLeft(Y)) //Plot Y axis (scaleLinear) to the left
+//     // .attr('fill', '#ffffff20')
+//       .selectAll('text')
+//       // .attr('fill', 'white')
     
 
-  return { X, Y};
-}
+//   return { X, Y};
+// }
+
+//setup scales
+const xscale = d3.scaleBand().range([0, dimensions.width]).paddingInner(0.2)
+const yscale = d3.scaleLinear().range([dimensions.height, 0])
+
+//setup axis
+const xaxis = d3.axisBottom(xscale)
+const group_xaxis = SVG.append('g').attr('class', 'x axis')
+  group_xaxis.attr('transform', `translate(0, ${dimensions.height})`)
+
+const yaxis = d3.axisLeft(yscale)
+const group_yaxis = SVG.append('g').attr('class', 'y axis')
+
 
 /**
  * @description Render graph with bars and scales
  * @param {JSON} data data from an API endpoint, specifically data sanitized by sanitizeData()
  */
 async function render(data) {
-  const svg = d3.select('svg')
-  const {X, Y} = createAxis(data)
+  // const svg = d3.select('svg')
+  xscale.domain(data.map(d => d.name))
+  yscale.domain([0, d3.max(data, d=> {return d.rating})])
+  // render the axis
+  group_xaxis.transition().call(xaxis)
+    .selectAll('text')
+    .attr('transform', `translate(-10,0) rotate(-45)`)
+    .style('text-anchor', 'end')
+  group_yaxis.transition().call(yaxis);
+  // const {X, Y} = createAxis(data)
 
-  SVG
+  const rec = SVG
     .selectAll('rect')
     .data(data)
     .join(
       (enter) => {
         const r = enter.append('rect')
         r
-        .style('opacity',0.5)
+        .style('opacity',1)
         .append('title')
         return r
     },
     (update) => {
-      return update.style('opacity',1)
+      return update.transition().style('fill', '#FFCC00')
     },
     (exit) => {return exit.remove()})
-    .attr('class', 'bar') //Why give it a class?
-    .attr('x', (d) => {return X(d.name)})
-    .attr('width', X.bandwidth())
-    .transition()
-    .attr('y', (d) => {return Y(d.rating)})
-    .transition()
-      .ease(d3.easeLinear)
-      .delay((d,i) => {return i*80})
-      .attr('height', (d) => {return dimensions.height - Y(d.rating)})  
-      .select('title').text(d => {return `${d.name}: ${d.rating}`})
 
-  SVG
-    .selectAll('p')
+
+    rec
+      .transition()
+      .ease(d3.easeQuadIn)
+      .delay((d,i) => {return i*80})
+        .attr('class', 'bar') //Why give it a class?
+        .attr('x', (d) => {return xscale(d.name)})
+        .attr('width', xscale.bandwidth())
+        .attr('y', (d) => {return yscale(d.rating)})
+        .transition()
+          .attr('height', (d) => {return dimensions.height - yscale(d.rating)})  
+          .select('title').text(d => {return `${d.name}: ${d.rating}`})
+
+  const rec_num = SVG
+    .selectAll('.text')
     .data(data)
     .join(
       (enter) => {
@@ -115,19 +140,23 @@ async function render(data) {
         return exit.remove()
       }
     )
+
     // .append('text')
-    .text((d) => {return d.rating})
-    .attr('x', (d) => {return X(d.name) + X.bandwidth() / 2})
-    .attr('y', (d) => {return Y(d.rating) + 14})
-    .attr('opacity', 0)
-    .attr('font-size', '11px')
-    .attr('fill', 'white')
-    .attr('text-anchor', 'middle')
+    rec_num
+      .attr('opacity', 0)
       .transition()
-      .ease(d3.easeQuadIn)
-      .delay((d,i) => {return i*100})
-      .attr('opacity', 1)
-}
+      .text((d) => {return d.rating})
+      .attr('x', (d) => {return xscale(d.name) + xscale.bandwidth() / 2})
+      .attr('y', (d) => {return yscale(d.rating) + 14})
+      .attr('font-size', '11px')
+      .attr('fill', 'white')
+      .attr('text-anchor', 'middle')
+        .transition()
+        .ease(d3.easeQuadIn)
+        .delay((d,i) => {return i*100})
+        .attr('opacity', 1)
+
+      }
 
 const pages = {
   first: await sanitizeData(await dataset(API)),
